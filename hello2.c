bf8eea7fb151b49ddb948c10cef78e3ee0b8aa02
Jake Erwin <Jakerwin7@huskers.unl.edu>
2022-08-29 01:38:34 +0000

    initial commit

---
+++ hello2.c
@@ -0,0 +1,11 @@
+/**
+ *  Basic Hello World Program
+ */
+
+int main(int argc, char **argv)  {
+
+        printf("Hello, I'm Jake Erwin!/n");
+        printf("My major is Computer Science!/n");
+
+        return 0;
+}
