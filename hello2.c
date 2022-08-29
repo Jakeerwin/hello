11bdce2c0f7ea845ac49a918c4225198e093b63f
Jake Erwin <Jakerwin7@huskers.unl.edu>
2022-08-29 02:07:24 +0000

    changed the greeting

--- hello2.c
+++ hello2.c
@@ -5,7 +5,7 @@
 #include <stdio.h>
 int main(int argc, char **argv)  {
 
-        printf("Hello, I'm Jake Erwin!/n");
+        printf("Hi, I'm Jake Erwin!/n");
         printf("My major is Computer Science!/n");
 
         return 0;
