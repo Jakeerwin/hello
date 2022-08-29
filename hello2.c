cbd8226df708ae004518019cf4dc6adba5beace0
Leo-Ray465 <leotray@live.com>
2022-08-29 03:08:38 +0000

    added myself and my major

--- hello2.c
+++ hello2.c
@@ -8,5 +8,7 @@ int main(int argc, char **argv)  {
         printf("Hi, I'm Jake Erwin!/n");
         printf("My major is Computer Science!/n");
 
+        printf("My name is Leo Ray, my major is computer science");
+
         return 0;
 }
